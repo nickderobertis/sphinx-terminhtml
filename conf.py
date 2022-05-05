378f7152e2f1af4ab8f4be921f74a28941d79f09
Nick DeRobertis <whoopnip@gmail.com>
2022-05-05 16:40:57 -0400

    Add requests to required packages

--- conf.py
+++ conf.py
@@ -63,6 +63,7 @@ PACKAGE_INSTALL_REQUIRES = [
     # 'package',
     # 'otherpackage>=1,<2'
     "terminhtml",
+    "requests",
 ]
 
 # Add any third party packages you use in requirements for optional features of your package here
